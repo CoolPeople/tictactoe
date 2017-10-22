a5bb6d42abbca8ba680e263f7f42df510592b21c
Olafur Karl Stefansson <oli3kalli@gmail.com>
2017-10-22 16:20:16 +0000

    Added initial classes and tests

---
+++ src/main/java/is/ru/coolpeople/tictactoe/Player.java
@@ -0,0 +1,9 @@
+package is.ru.coolpeople.tictactoe;
+
+/**
+ * Created by olafur on 22/10/2017.
+ */
+
+public class Player {
+	//todo: implement player functionality
+}
---
+++ src/test/java/is/ru/coolpeople/tictactoe/PlayerTest.java
@@ -0,0 +1,8 @@
+package is.ru.coolpeople.tictactoe;
+
+/**
+ * Created by olafur on 22/10/2017.
+ */
+public class PlayerTest {
+	//todo: implement player tests
+}
