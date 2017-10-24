252c20ffc152ec56a3be042f0d9ee3f4daf44717
annakrih <annah14@ru.is>
2017-10-24 11:51:47 +0000

    doTurn made and tested

--- src/main/java/is/ru/coolpeople/tictactoe/Game.java
+++ src/main/java/is/ru/coolpeople/tictactoe/Game.java
@@ -3,16 +3,37 @@ package is.ru.coolpeople.tictactoe;
 /**
  * Created by olafur on 22/10/2017.
  */
+import java.util.Queue;
 
 public class Game {
 	//Todo: Implement game class
     private Board board;
+    private Queue<Player> players; // The current player is always first in the Queue
     
-    
-    public Game () {
+    public Game (Queue<Player> playersQueue) {
         board = new Board();
+        players = playersQueue;
     }
     
+    // Returns false if the cell has already been marked, otherwise, markes the cell
+    public boolean doTurn (int index) {
+        if(board.getSymbolAtIndex(index) != null) {
+            return false;
+        }
+        
+        board.placeSymbol( players.peek().getSymbol(), index);
+        players.add( players.poll() ); //changes the current player
+        return true;
+    }
+    
+    public Board getBoard() {
+        return board;
+    }
+    
+    public String currentPlayerName()
+    {
+        return (players.peek()).getName();
+    }
 
     
 }
--- src/test/java/is/ru/coolpeople/tictactoe/GameTest.java
+++ src/test/java/is/ru/coolpeople/tictactoe/GameTest.java
@@ -1,22 +1,74 @@
 package is.ru.coolpeople.tictactoe;
 
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+
 import org.junit.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.Assert.*;
 
 /**
  * Created by olafur on 22/10/2017.
  */
 
-@SpringBootTest
+//@SpringBootTest
 public class GameTest {
     
     @Test
     public void testMakesGame ()
     {
-        Game g = new Game();
-        assertFalse(g == null);
+        Player p1 = new Player ("Anna", "X");
+        Player p2 = new Player ("Hafsteinn", "O");
+        Queue<Player> players = new ArrayBlockingQueue<Player>(2);
+        players.add(p1);
+        players.add(p2);
+        Game g = new Game(players);
+        assertTrue(g != null);
     }
     
+    @Test
+    public void testPlayers ()
+    {
+        Player p1 = new Player ("Anna", "X");
+        Player p2 = new Player ("Hafsteinn", "O");
+        Queue<Player> players = new ArrayBlockingQueue<Player>(2);
+        players.add(p1);
+        players.add(p2);
+        Game g = new Game(players);
+        assertEquals("Anna", g.currentPlayerName());
+        assertEquals("Anna", g.currentPlayerName());
+
+    }
+    
+    
+    @Test
+    public void testDoTurn ()
+    {
+        Player p1 = new Player ("Anna", "X");
+        Player p2 = new Player ("Hafsteinn", "O");
+        Queue<Player> players = new ArrayBlockingQueue<Player>(2);
+        players.add(p1);
+        players.add(p2);
+        Game g = new Game(players);
+        
+        assertEquals("Anna", g.currentPlayerName());
+        assertEquals(true, g.doTurn(0)); //Anna
+        assertEquals("Hafsteinn", g.currentPlayerName());
+        assertEquals(true, g.doTurn(1)); //Hafsteinn
+        assertEquals("Anna", g.currentPlayerName());
+        assertEquals(false, g.doTurn(1)); //Anna
+        assertEquals("Anna", g.currentPlayerName());
+        assertEquals(true, g.doTurn(2)); //Anna
+        assertEquals("Hafsteinn", g.currentPlayerName());
+        assertEquals(false, g.doTurn(1)); //Hafsteinn
+        
+        Board b = g.getBoard();
+        String[] grid = b.getGrid();
+        
+        assertTrue(grid[0] == "X");
+        assertTrue(grid[1] == "O");
+        assertTrue(grid[2] == "X");
+    }
     
 
 }
