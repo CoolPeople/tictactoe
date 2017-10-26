41b24bc272588fe974a1aca1d57838f9b5eaee87
Ólafur Karl Stefánsson <olafur@trackwell.com>
2017-10-26 17:34:50 +0000

    Added new functionality to detect which port Heroku wants to use, and uses it

--- src/main/java/is/ru/coolpeople/ServletInitializer.java
+++ src/main/java/is/ru/coolpeople/ServletInitializer.java
@@ -3,11 +3,12 @@ package is.ru.coolpeople;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
+import java.util.HashMap;
+
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(TicTacToeApplication.class);
 	}
-
 }
