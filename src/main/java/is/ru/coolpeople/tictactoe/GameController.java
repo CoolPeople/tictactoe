35624e1c9bbd05ec6a218fe87a5e0f8f8567da2a
Olik <oli3kalli@gmail.com>
2017-10-28 20:30:13 +0000

    Made new controller functionality to properly hook up the front end.

--- src/main/java/is/ru/coolpeople/tictactoe/GameController.java
+++ src/main/java/is/ru/coolpeople/tictactoe/GameController.java
@@ -1,20 +1,12 @@
 package is.ru.coolpeople.tictactoe;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import java.util.HashMap;
-import java.util.Queue;
-import java.util.concurrent.ArrayBlockingQueue;
 
 @RestController
 public class GameController {
-    static HashMap<String, Queue<Player>> players;
     static HashMap<String, Game> games;
 
     @RequestMapping(value = "/doTurn")
@@ -29,27 +21,10 @@ public class GameController {
         game.doTurn(slot);
     }
 
-    @PostMapping(value = "/newPlayer")
-    public ResponseEntity<?> newPlayer(@RequestParam("name") String name,
-                                       @RequestParam("symbol") String symbol,
-                                       HttpSession session){
-        Player player = new Player(name, symbol);
-        players.computeIfAbsent(session.getId(),
-                k -> new ArrayBlockingQueue<>(2));
-        players.get(session.getId()).add(player);
-
-        return new ResponseEntity<>(player, HttpStatus.OK);
+    @PostMapping(value = "/newGame", headers = "Accept=application/json", consumes="application/json")
+    public void newGame(@RequestBody NewGameWrapper wrapper, HttpSession session) {
+        Game game = new Game(wrapper.getPlayers());
+        games.put(session.getId(), game);
     }
-
-    @PostMapping(value = "/newGame")
-    public ResponseEntity<?> newGame(HttpSession session){
-        Queue<Player> sessionPlayers = players.get(session.getId());
-        if (sessionPlayers == null || sessionPlayers.size() < 2) {
-            throw new IllegalStateException("Not enough players for a new game");
-        }
-        games.put(session.getId(), new Game(sessionPlayers));
-        return new ResponseEntity<>("success", HttpStatus.OK);
-    }
-
 
 }
