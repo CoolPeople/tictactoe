174ec03b118ece101675abb4aa7cf4dc7470468d
bjornt16 <bjornt16@ru.is>
2017-10-29 21:54:16 +0000

    tests

--- src/selenium/java/is/ru/coolpeople/tictactoe/TestTicTacToe.java
+++ src/selenium/java/is/ru/coolpeople/tictactoe/TestTicTacToe.java
@@ -1,7 +1,8 @@
 package is.ru.coolpeople.tictactoe;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 import org.junit.Test;
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -14,15 +15,61 @@ public class TestTicTacToe extends SeleniumTestWrapper {
   }
 
   @Test
-  public void testSimpleAdd() throws Exception {
+  public void testPlayerOneName() throws Exception {
     driver.get(baseUrl);
     /* Remove Thread.sleep... */
-    Thread.sleep(2000);
-    //WebElement input = driver.findElement(By.id("number"));
-    //input.sendKeys("1");
-    assertEquals(0,0);
-    /* Remove Thread.sleep... */
-    Thread.sleep(2000);
-    /* ... finish test! */
+    WebElement input = driver.findElement(By.id("pcOneName"));
+    input.sendKeys("Adam");
+    assertEquals("Adam", input.getAttribute("value"));
   }
+
+  @Test
+  public void testPlayerCountAndClick() throws Exception {
+    driver.get(baseUrl);
+    /* Remove Thread.sleep... */
+    WebElement input = driver.findElement(By.xpath("//input[@name='playerCount']"));
+    assertEquals("1", input.getAttribute("value"));
+    driver.findElement(By.id("pcTwo")).click();
+    assertEquals("2 ", input.getAttribute("value"));
+
+    WebElement psTwoDiv = driver.findElement(By.className("psTwo"));
+    assertTrue(psTwoDiv.getAttribute("class").contains("show"));
+  }
+
+  @Test
+  public void testTwoPlayerGame() throws Exception {
+    driver.get(baseUrl);
+    /* Remove Thread.sleep... */
+    driver.findElement(By.id("pcTwo")).click();
+    WebElement input = driver.findElement(By.xpath("//input[@name='playerCount']"));
+    assertEquals("2 ", input.getAttribute("value"));
+
+
+    WebElement input2 = driver.findElement(By.id("pcOneName"));
+    input2.sendKeys("Adam");
+    assertEquals("Adam", input2.getAttribute("value"));
+
+    WebElement input3 = driver.findElement(By.id("pcTwoName"));
+    input3.sendKeys("Bob");
+    assertEquals("Bob", input3.getAttribute("value"));
+
+    driver.findElement(By.className("startGame")).click();
+
+    WebElement input4 = driver.findElement(By.id("menuWrapper"));
+    assertFalse(input4.getAttribute("class").contains("show"));
+
+
+    List<WebElement> gameTiles =  driver.findElements(By.className("gameTile"));
+    // click the 3rd checkbox
+    gameTiles.get(0).click();
+    gameTiles.get(3).click();
+    gameTiles.get(1).click();
+    gameTiles.get(4).click();
+    gameTiles.get(2).click();
+
+    WebElement input5 = driver.findElement(By.id("gameOver"));
+    assertTrue(input5.getAttribute("class").contains("show"));
+
+  }
+
 }
