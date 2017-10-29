474330ecd59c9c1aa29ec565c1e836309c73e52b
Ólafur Karl Stefánsson <olafur@trackwell.com>
2017-10-29 18:17:06 +0000

    Made selenium run.

--- src/selenium/java/is/ru/coolpeople/tictactoe/SeleniumTestWrapper.java
+++ src/selenium/java/is/ru/coolpeople/tictactoe/SeleniumTestWrapper.java
@@ -8,24 +8,24 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 
 public abstract class SeleniumTestWrapper {
-	  static ChromeDriver driver;
-	    static String baseUrl;
-	      static String port;
-	      
-	        @BeforeClass
-		  public static void openBrowser() {
-			      driver = new ChromeDriver();
-			          driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-				  
-				      port = System.getenv("PORT");
-				          if (port == null) {
-						          port = "4567";
-							      }
-					      baseUrl = "http://localhost:" + port;
-					        }
-		
-		  @AfterClass
-		    public static void closeBrowser(){
-			        driver.quit();
-				  }
+    static ChromeDriver driver;
+    static String baseUrl;
+    static String port;
+
+    @BeforeClass
+    public static void openBrowser() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        port = System.getenv("PORT");
+        if (port == null) {
+            port = "4567";
+        }
+        baseUrl = "http://localhost:" + port;
+    }
+
+    @AfterClass
+    public static void closeBrowser() {
+        driver.quit();
+    }
 }
